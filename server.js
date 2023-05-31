b3b41be1fa1e12b17cc2f20b6d2d43d3e1124679
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-05-31 14:37:36 +0530

    printing my name on the browser using node js

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const http = require('http');
 
 // function listener(req,res){
-
 // }
 // http.createServer(listener);
-
 // http.createServer(function(req,res){
 
 // });
+// const server=http.createServer((req,res)=>{
+//     console.log('Himanshu Singh Patel');
+// });
+// server.listen(4000);
 
 const server=http.createServer((req,res)=>{
-    console.log('Himanshu Singh Patel');
+    console.log(req.url,req.method,req.headers);
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>First Page Using Node JS</title></body>');
+    res.write('<body><h1>Himanshu Singh Patel</h1></body>');
+    res.write('</html>');
+    res.end();
+    
 });
 server.listen(4000);
-
 
