3dca3c6565d8e4619da0b7034196ecb548c30930
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-05-31 18:05:38 +0530

    hiting different url different page pop up

--- server.js
+++ server.js
@@ -1,25 +1,38 @@
 const http = require('http');
-
-// function listener(req,res){
-// }
-// http.createServer(listener);
-// http.createServer(function(req,res){
-
-// });
-// const server=http.createServer((req,res)=>{
-//     console.log('Himanshu Singh Patel');
-// });
-// server.listen(4000);
+const fs=require('fs');
 
 const server=http.createServer((req,res)=>{
-    console.log(req.url,req.method,req.headers);
+    const url=req.url;
+    const method=req.method;
+    if(url==="/"){
+      res.write('<html>');
+      res.write('<head><title>First Page Using Node JS</title></body>');
+      res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+      res.write('</html>');
+      return res.end();
+    }   
+    if(url==="/home"){
+      res.write('<html>');
+      res.write('<head><title>First Page Using Node JS</title></body>');
+      res.write('<body><h1>Welcome Home</h1></body>');
+      res.write('</html>');
+      return res.end();
+    } 
+    if(url==="/about"){
+      res.write('<html>');
+      res.write('<head><title>First Page Using Node JS</title></body>');
+      res.write('<body><h1>Welcome to About Us page</h1></body>');
+      res.write('</html>');
+      return res.end();
+    }   
+    if(url==="/node"){
+      res.write('<html>');
+      res.write('<head><title>First Page Using Node JS</title></body>');
+      res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+      res.write('</html>');
+      return res.end();
+    } 
     res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<head><title>First Page Using Node JS</title></body>');
-    res.write('<body><h1>Himanshu Singh Patel</h1></body>');
-    res.write('</html>');
-    res.end();
-    
 });
 server.listen(4000);
 
